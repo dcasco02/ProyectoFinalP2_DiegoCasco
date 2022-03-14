810fe013ddfdf3672333c419e334a28f4e79fa8a
dcasco02 <dcasco@unitec.edu>
2022-03-13 22:13:31 -0600

    Cliente mod

--- src/proyectofinalprograii/Cliente.java
+++ src/proyectofinalprograii/Cliente.java
@@ -12,16 +12,30 @@ package proyectofinalprograii;
 public class Cliente {
     private String nombre;
     private int identidad;
+    private String CorreoElec;
     private boolean discapacidad;
     private int cantidadtrans;
-
+    private String tipotrans;
+    
+       
     public Cliente() {
     }
 
-    public Cliente(int identidad, boolean discapacidad, int cantidadtrans) {
+    public Cliente(String nombre, int identidad, String CorreoElec, boolean discapacidad, int cantidadtrans, String tipotrans) {
+        this.nombre = nombre;
         this.identidad = identidad;
+        this.CorreoElec = CorreoElec;
         this.discapacidad = discapacidad;
         this.cantidadtrans = cantidadtrans;
+        this.tipotrans = tipotrans;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public int getIdentidad() {
@@ -46,6 +60,22 @@ public class Cliente {
 
     public void setCantidadtrans(int cantidadtrans) {
         this.cantidadtrans = cantidadtrans;
+    }
+
+    public String getTipotrans() {
+        return tipotrans;
+    }
+
+    public void setTipotrans(String tipotrans) {
+        this.tipotrans = tipotrans;
+    }
+
+    public String getCorreoElec() {
+        return CorreoElec;
+    }
+
+    public void setCorreoElec(String CorreoElec) {
+        this.CorreoElec = CorreoElec;
     }
     
     
